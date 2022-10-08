c4b825390be317ffe1c445f659545e6eeee6bbc2
rockysaikia730 <rockysaikia98640@gmail.com>
2022-10-08 20:00:45 +0530

    Features added

--- Grammer_Tools/UI.py
+++ Grammer_Tools/UI.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from tkinter.filedialog import askopenfilename
 from tkinter import filedialog
+
+from main import *
 #import functions from main
 
 root = Tk()
@@ -10,17 +12,50 @@ root.title("Grammer Tools")
 
 def open_file():
     #take file from the user as input
-    file_type = ['''set file type as .txt''']
-    filename = filedialog.askopenfilename(filetypes=file_type)
+    file_type = ['.txt']
+    file = filedialog.askopenfile()
     #next read the content from the file and pass it to get_text()
+    if file is not None:
+        get_text(file.read()) 
 
 #Create an input box to take 'text' as input from the user
 
-def call_find_noun():
-    pass
+def call_find_feature(feature):
+    if feature == "Noun":
+        return find_noun
+    elif feature == "Pronoun":
+        return find_pronoun
+    elif feature == "Adverb":
+        return find_adverbs
+    elif feature == "Adjective":
+        return find_adjectives
+    elif feature == "Preposition":
+        return find_preposition
+    elif feature == "Conjuction":
+        return find_conjuction
+    elif feature == "Interjection":
+        return find_interjection
+    elif feature == "Determiner":
+        return find_determiners
+    elif feature == "Punctuation":
+        return find_punctuation
+    
     #call find_noun function from main    
 
 #create an upload button for user to upload his text
 #create a button for calling open_file function
+open_btn = Button(root, text ='Open', command = open_file)
+open_btn.pack()
+
+#Stores all the feature finding buttons
+buttons = {}
+
+#List of all features
+feature_finder = ["Noun","Pronoun","Adverb","Adjective","Preposition","Conjuction",
+            "Interjection","Determiner","Punctuation"]
+
+for i in feature_finder:
+    buttons[i] = Button(root, text=i,command=call_find_feature(i))
+    buttons[i].pack()
 
 root.mainloop()
--- Grammer_Tools/main.py
+++ Grammer_Tools/main.py
@@ -1,4 +1,6 @@
 import spacy
+from collections import Counter
+
 npl=spacy.load("en_core_web_sm")
 
 def get_text(text):
@@ -6,9 +8,67 @@ def get_text(text):
     global doc
     doc = npl(text)
 
+#Parts of Speech Finder
+
+#Noun
 def find_noun():
     for x in doc:
-        if x.pos_=="NOUN":
+        if x.pos_=="NOUN" or x.pos_=="PROPN":
             print(x)
+
+#Pronoun
+def find_pronoun():
+    for x in doc:
+        if x.pos_ == "PRON":
+            print(x)
+
+#Adverb
+def find_adverbs():
+    for x in doc:
+        if x.pos_ == "ADV":
+            print(x)
+
+#Adjective
+def find_adjectives():
+    for x in doc:
+        if x.pos_ == "ADJ":
+            print(x)
+
+#Verb
+def find_verb():
+    for x in doc:
+        if x.pos_ == "VERB" or x.pos_ == "AUX":
+            print(x)
+
+#Preposition
+def find_preposition():
+    for x in doc:
+        if x.pos_ == "ADP":
+            print(x)
+
+#Conjuction     
+def find_conjuction():
+    for x in doc:
+        if x.pos_ == "CONJ" or x.pos_ == "SCON":
+            print(x)
+
+#Interjection
+def find_interjection():
+    for x in doc:
+        if x.pos_ == "INTJ":
+            print(x)
+
+#Punctuation
+def find_punctuation():
+    for x in doc:
+        if x.pos_ == "PUNCT":
+            print(x)
+
+#Articles/Determiners
+def find_determiners():
+    for x in doc:
+        if x.pos_ == "DET":
+            print(x)
+
 
 #create more functionality using space like find pronoun, adverbs etc.
